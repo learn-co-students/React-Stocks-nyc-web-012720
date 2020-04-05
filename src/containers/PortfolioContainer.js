08c45a9c8db958052d71ae67cb8747657722abd4
Natalia Wit <natwit95@gmail.com>
2020-04-05 19:25:13 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
-
+  renderPortfolio = () => {
+    // console.log(this.props)
+    return this.props.portfolioStocks.map((stock, index) => {
+  return <Stock key={`${index} ${stock.name}`} {...stock} handleClick={()=>this.props.sellStock(stock)}/>
+})
+  }
   render() {
+    // console.log(this.props.stocks)
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+          {this.renderPortfolio()}
+            
+          
       </div>
     );
   }
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class StockContainer extends Component {
-
+  renderStocks = () => {
+    return this.props.orgStocks.map(stock => <Stock key={stock.id} {...stock} handleClick={()=> this.props.buyStock(stock)}/>)
+  }
   render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {this.renderStocks()}
       </div>
     );
   }
