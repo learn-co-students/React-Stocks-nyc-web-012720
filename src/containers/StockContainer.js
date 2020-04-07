3f062717ec954519c294f7fd40cbec6235f45ce3
Alexandra Pestruyeva <apestruy@gmail.com>
2020-04-07 18:40:12 +0000

    Done.

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
-import Stock from '../components/Stock'
+import React, { Component } from "react";
+import Stock from "../components/Stock";
 
 class StockContainer extends Component {
+  // handleClick = (stock) => {
+  //   this.props.renderToPortfolio(stock);
+  // };
+
+  renderStocks = () => {
+    let stocksToRender = this.props.stocks;
+    return stocksToRender.map((stock) => {
+      return (
+        <Stock
+          stock={stock}
+          key={stock.id}
+          renderToPortfolio={this.props.renderToPortfolio}
+        />
+      );
+    });
+  };
 
   render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {this.renderStocks()}
       </div>
     );
   }
-
 }
 
 export default StockContainer;
