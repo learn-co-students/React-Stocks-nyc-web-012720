add61e50efe5890502b50f5a06802ae361acdf22
github username <github email address>
2020-04-07 20:03:50 -0400

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,21 @@
 import React from 'react'
 
-const Stock = () => (
-  <div>
-
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
+const Stock = (props) => (
+    <div>
+    
+    <div className="card" >
+      <div className="card-body" onClick={() => props.handleBuyStock ? props.handleBuyStock(props.stock) : props.handleSellStock(props.stockIndex)}>
+          <h5 className="card-title">{
+            props.stock.name
           }</h5>
-        <p className="card-text">{
-            //ticker: stock price
+          <p className="card-text">{
+          `${props.stock.ticker} ${props.stock.price}`
           }</p>
+        </div>
       </div>
+
+
     </div>
-
-
-  </div>
 );
 
 export default Stock
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,22 +3,70 @@ import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
+const API = 'http://localhost:3000/stocks'
+
 class MainContainer extends Component {
+  state = {
+    originalList: [],
+    stocks: [],
+    boughtStocks: [] 
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(response => response.json())
+    .then(data => this.setState({stocks: data, originalList: data}))
+  }
+
+  handleBuyStock = (stock) => {
+    this.setState(prevState => ({boughtStocks: [...prevState.boughtStocks, stock]}))
+  }
+
+  handleSellStock = (stockIndex) => {
+    const newBoughtStocks = this.state.boughtStocks.slice(0, stockIndex).concat(this.state.boughtStocks.slice((stockIndex + 1), this.state.boughtStocks.length -1))
+    this.setState({boughtStocks: newBoughtStocks})
+  }  
+
+  sortByPrice = () => {
+    this.setState({stocks: this.state.stocks.sort((a, b) => a.price > b.price ? 1 : -1)})
+  }
+
+  sortAlphabetically = () => {
+    this.setState({stocks: this.state.stocks.sort((a, b) => a.name > b.name ? 1 : -1)})
+  }
+
+  sortStocks = (e) => {
+    if (e.target.value === 'Alphabetically') {
+      return this.sortAlphabetically()
+    } else if (e.target.value === 'Price') {
+      return this.sortByPrice()
+    } 
+  }
+
+  filterByType = (e) => {
+    const filtered = this.state.originalList.filter(stock => stock.type === e.target.value)
+
+    if (e.target.value !== 'All') {
+      this.setState({stocks: filtered})
+    } else {
+      this.setState(prevState => ({stocks: prevState.originalList}))
+    }
+  }
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar sortStocks={this.sortStocks} filterByType={this.filterByType}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+            <StockContainer stocks={this.state.stocks} handleBuyStock={this.handleBuyStock} />
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+            <PortfolioContainer boughtStocks={this.state.boughtStocks} handleSellStock={this.handleSellStock}/>
 
             </div>
           </div>
